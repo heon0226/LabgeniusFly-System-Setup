eb9dfe7e0ff348d1e9a7e86b7f414c8e45b4002b
길병헌 <heon@gilbyeongheon-ui-MacBookAir.local>
2023-02-22 16:00:06 +0900

    super user do nodejs repo script

--- l6470_setup.sh
+++ l6470_setup.sh
@@ -32,7 +32,7 @@ pip3 install pyzmq smbus hid zmq gpio
 pip3 install flask flask-restful flask-cors
 
 # Install nodejs and npm 
-curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo bash -
 sudo apt install -y nodejs npm
 
 echo "NodeJS Package Installing"
